1a67f1ff1a476d0118a0dec9dda73800eba56c68
Mayank kshirsagar <user.email like:"mayank9180@gmail.com>
2025-04-23 15:56:37 +0530

    gunicorn functionality added

--- server/app.py
+++ server/app.py
@@ -103,6 +103,5 @@ def serve_react_app(path):
         return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
+    from app import app  # Only if your app instance is inside app.py
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
